00d9bf90cd06f0295ebb17fd9c1948eb38dd2419
Davide Ciraudo <davideciraudo@MacBook-Air-di-Davide.local>
2023-09-20 21:09:59 +0200

    navbar terminata

---
+++ main.js
@@ -0,0 +1,10 @@
+let navbar= document.querySelector("#navbar")
+
+window.addEventListener("scroll", ()=>{
+    // console.log(window.scrollY);
+    if (window.scrollY > 0) {
+        navbar.classList.add("navbar-custom")
+    } else{
+        navbar.classList.remove("navbar-custom")
+    }
+})
