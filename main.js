81ece2b57b55937b0a74fc29e7f4274b312612c3
Davide Ciraudo <davideciraudo@MacBook-Air-di-Davide.local>
2023-09-21 17:00:56 +0200

    fine esercitazione Inizio Presonalizzazione GODMODE

--- main.js
+++ main.js
@@ -47,3 +47,55 @@ let observer= new IntersectionObserver((entries)=>{
 
 observer.observe(articlesNumber)
 
+let annunci = [
+    {nome: "Martelletti", categoria:"Accorderia", prezzo:"50$", url:"https://picsum.photos/201"},
+    {nome: "Tastiera", categoria:"PressureTecnology", prezzo:"200$", url:"https://picsum.photos/202"},
+    {nome: "Corde", categoria:"Accorderia", prezzo:"1.000$", url:"https://picsum.photos/203"},
+    {nome: "Pedali", categoria:"PressureTecnology", prezzo:"150$", url:"https://picsum.photos/204"},
+    {nome: "VISCOUNT Opera 450", categoria:"Pianoforte Acustico", prezzo:"37.000$", url:"https://picsum.photos/205"},
+]
+
+let annunciWrapper= document.querySelector("#annunciWrapper")
+
+annunci.forEach((annuncio, i)=>{
+    if (i>= annunci.length - 3){
+
+     let div = document.createElement("div")
+    div.classList.add("col-12", "col-md-4", "col-lg-3", "my-5")
+    div.innerHTML=`
+                  <div class="card mt-5">
+                    <div class="overflow-hidden">
+                        <img src=${annuncio.url} class="card-img-top transition" alt="...">  
+
+                    </div>
+                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                     New
+                        <span class="visually-hidden">unread messages</span>
+                      </span>
+                    <div class="card-body">
+                      <h5 class="card-title">${annuncio.nome}</h5>
+                      <p class="card-text">${annuncio.categoria}</p>
+                      <p class="card-text fw-bold">${annuncio.prezzo}</p>
+                      <div class="d-flex justify-content-between fs-4">
+                        <a href="#" class="btn btn-outline-danger">Aggiungi al Carrello</a>
+                        <i class="bi bi-heart"></i>
+                      </div>
+                      
+                      <p class="card-text mt-3 text-end"><small class="text-body-secondary ">Last updated 3 mins ago</small></p>
+                    </div>
+                  </div>
+    `
+    annunciWrapper.appendChild(div)   
+    }
+    
+})
+
+let heart = document.querySelectorAll(".bi-heart");
+
+heart.forEach((cuori)=>{
+    cuori.addEventListener("click", ()=>{
+        cuori.classList.toggle("bi-heart")
+        cuori.classList.toggle("bi-heart-fill")
+        cuori.classList.toggle("text-danger")
+    })
+})
